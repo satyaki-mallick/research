ed3008c14d5f8ba4b63642af74bd13eceb32aee1
Ryan Morton <mortonanalytics@gmail.com>
2019-04-08 14:39:37 -0600

    initialize simulation; abstract label; add documentation

--- exploratory/rMorton/d3-graphNet/d3-graphNet.js
+++ exploratory/rMorton/d3-graphNet/d3-graphNet.js
@@ -4,6 +4,26 @@
 class graphNet {
 	
 	constructor(opts) {
+		//example opts object
+		/*
+		const opts = {
+			element: document.getElementById("sourceCred"),
+			data: myData,
+			mapping: {
+				nodes: 'nodes',
+				nodeLable: 1,
+				nodeGroup: null,
+				nodeSize: null,
+				edges: 'edges',
+				edgeGroup: null,
+				edgeSize: null,
+				source: null,
+				target: null
+			},
+			options: null
+		}
+		*/
+		
 		// load arguments from config object
 		this.element = opts.element;
 		this.data = opts.data;
@@ -15,10 +35,12 @@ class graphNet {
 	}
 	
 	draw(){
-  	
+		//preserve context for closures
+		var that = this;
+		
 		// define dimensions
 		this.width = this.element.offsetWidth;
-		this.height = 1960; //this.element.offsetHeight;
+		this.height = 640; //this.element.offsetHeight;
 		
 		// set up parent element and SVG
 		this.element.innerHTML = '';
@@ -28,20 +50,20 @@ class graphNet {
 		this.chart = this.svg
 			.append("g")
 			.attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")")
+			
+		// set up svg defs
+		this.chart.append("defs").append("marker")
+				.attr("id", "arrow")
+				.attr("viewBox", "0 -3 10 10")
+				.attr("refX", 18)
+				.attr("refY", 0)
+				.attr("markerWidth", 5)
+				.attr("markerHeight", 5)
+				.attr("orient", "auto")
+				.append("svg:path")
+				.attr("d", "M 0,-5 L 10 ,0 L 0,5");   
 		
-    // set up svg defs
-    this.chart.append("defs").append("marker")
-            .attr("id", "arrow")
-            .attr("viewBox", "0 -3 10 10")
-            .attr("refX", 18)
-            .attr("refY", 0)
-            .attr("markerWidth", 5)
-            .attr("markerHeight", 5)
-            .attr("orient", "auto")
-            .append("svg:path")
-            .attr("d", "M 0,-5 L 10 ,0 L 0,5");   
-    
-    //chart elements        
+		//chart elements        
 		this.edge = this.chart.append("g");
 		this.node = this.chart.append("g");
 		this.tooltip = d3.select(this.element).append("div")
@@ -51,10 +73,44 @@ class graphNet {
 			.style('min-width' , '50px')
 			.style('height', 'auto')
 			.style('background', 'none repeat scroll 0 0 #ffffff')
+		
+		//initialize simulation
+		///define variables from data for simulation only
+		///data updates chart and restarts simulation later
+		const links = this.data[this.mapping.edges];
+		if(that.mapping.source){
+			links.forEach(function(d){
+				d.source = d[that.mapping.source];
+				d.target = d[that.mapping.target];
+			});
+		}
+    
+		const nodes = this.data[this.mapping.nodes];
+		
+		/// define simulation object
+		this.simulation = d3.forceSimulation(nodes)
+			.force("charge", d3.forceManyBody().strength(-1500))
+			.force("link", d3.forceLink(links).distance(100))
+			.force("collide", d3.forceCollide().radius(2))
+			.force("x", d3.forceX())
+			.force("y", d3.forceY())
+			.alphaTarget(1)
+			.on("tick", ticked);
+			
+		function ticked() {
+		    that.node.selectAll('.node').attr("cx", function(d) { return d.x; })
+				.attr("cy", function(d) { return d.y; })
+			
+			//TODO: fix arrow marker by moving back based on the node radius
+		    that.edge.selectAll('.edge').attr("x1", function(d) { return d.source.x; })
+				.attr("y1", function(d) { return d.source.y; })
+				.attr("x2", function(d) { return d.target.x; })
+				.attr("y2", function(d) { return d.target.y; });
+		}
     
 		//this.setColorScales();
 		this.nodeColor = d3.scaleOrdinal(d3.schemeCategory10);
-    this.edgeColor = d3.scaleOrdinal(d3.schemeCategory10);
+		this.edgeColor = d3.scaleOrdinal(d3.schemeCategory10);
 		// update chart
 		this.update(this.data);
 	}
@@ -66,124 +122,99 @@ class graphNet {
     
 		//define variables from data
 		const links = data[this.mapping.edges];
-    if(that.mapping.source){
-    	console.log("link rename")
-    	links.forEach(function(d){
-        d.source = d[that.mapping.source];
-        d.target = d[that.mapping.target];
-    	});
-    }
-    console.log(links);
+		if(that.mapping.source){
+			links.forEach(function(d){
+			d.source = d[that.mapping.source];
+			d.target = d[that.mapping.target];
+			});
+		}
+    
 		const nodes = data[this.mapping.nodes];
 		
-		// define simulation object
-		const simulation = d3.forceSimulation(nodes)
-			.force("charge", d3.forceManyBody().strength(-150))
-			.force("link", d3.forceLink(links).distance(2))
-			.force("collide", d3.forceCollide().radius(2))
-			.force("x", d3.forceX())
-			.force("y", d3.forceY())
-			.alphaTarget(1)
-			.on("tick", ticked);
-		
 		// restart simulation with new data
-		restart(nodes, links);
-		
-		function restart(nodes, links){
-    	
-			// node data join
-			var node = that.node.selectAll(".node").data(nodes);
-   
-			// node exit	
-			node.exit()
-				.transition()
-				.ease(d3.easeQuad)
-				.duration(1000)
-				.remove();
-			
-			//node enter
-			var newNode = node.enter()
-				.append('circle')
-        .attr('class', 'node')
-        .on('mouseover', mouseOver)
-				.on('mouseout', mouseOff);
-       
-			// node update	
-			node.merge(newNode)
-				.transition()
-				.ease(d3.easeQuad)
-				.duration(1000)
-				.attr('fill', function(d){
-					if(that.mapping.nodeGroup){
-						return that.nodeColor(d[that.mapping.nodeGroup]);
-					} else {
-						return 'steelblue';
-					}
-				})
-				.attr('r', function(d){
-					if(that.mapping.nodeSize){
-						return d[that.mapping.nodeSize];
-					} else {
-						return 5;
-					}
-				});
-			
-			// edge data join
-			var edge = that.edge.selectAll('.edge').data(links);
-			
-			// edge exit
-			edge.exit().remove();
-			
-			// edge enter
-			var newEdge = edge.enter()
-				.append('line')
-				.attr('class', 'edge');
-			
-			edge.merge(newEdge)
-				.transition()
-				.ease(d3.easeQuad)
-				.duration(1000)
-				.attr("marker-end", "url(#arrow)")
-				.attr('stroke-width', function(d){
-					if(that.mapping.edgeSize){
-						return d[that.mapping.edgeSize];
-					} else {
-						return '#000';
-					}
-				})
-				.attr('stroke', function(d){
-					if(that.mapping.edgeGroup){
-						return that.edgeColor(d[that.mapping.edgeGroup]);
-					} else {
-						return '#000';
-					}
-				});
-				
-			// Update and restart the simulation.
-		    simulation.nodes(nodes);
-		    simulation.force("link").links(links);
-		    simulation.alpha(1).restart();
-		}
-			
-		function ticked() {
-		  that.node.selectAll('.node').attr("cx", function(d) { return d.x; })
-			  .attr("cy", function(d) { return d.y; })
+		// node data join
+		var node = that.node.selectAll(".node").data(nodes);
 
-		  that.edge.selectAll('.edge').attr("x1", function(d) { return d.source.x; })
-			  .attr("y1", function(d) { return d.source.y; })
-			  .attr("x2", function(d) { return d.target.x; })
-			  .attr("y2", function(d) { return d.target.y; });
-		}
+		// node exit	
+		node.exit()
+			.transition()
+			.ease(d3.easeQuad)
+			.duration(1000)
+			.remove();
+			
+		//node enter
+		var newNode = node.enter()
+			.append('circle')
+			.attr('class', 'node')
+			.on('mouseover', mouseOver)
+			.on('mouseout', mouseOff);
+       
+		// node update	
+		node.merge(newNode)
+			.transition()
+			.ease(d3.easeQuad)
+			.duration(1000)
+			.attr('fill', function(d){
+				if(that.mapping.nodeGroup){
+					return that.nodeColor(d[that.mapping.nodeGroup]);
+				} else {
+					return 'steelblue';
+				}
+			})
+			.attr('r', function(d){
+				if(that.mapping.nodeSize){
+					return d[that.mapping.nodeSize];
+				} else {
+					return 5;
+				}
+			});
+			
+		// edge data join
+		var edge = that.edge.selectAll('.edge').data(links);
+		
+		// edge exit
+		edge.exit().remove();
+		
+		// edge enter
+		var newEdge = edge.enter()
+			.append('line')
+			.attr('class', 'edge');
+		
+		edge.merge(newEdge)
+			.transition()
+			.ease(d3.easeQuad)
+			.duration(1000)
+			.attr("marker-end", "url(#arrow)")
+			.attr('stroke-width', function(d){
+				if(that.mapping.edgeSize){
+					return d[that.mapping.edgeSize];
+				} else {
+					return '#000';
+				}
+			})
+			.attr('stroke', function(d){
+				if(that.mapping.edgeGroup){
+					return that.edgeColor(d[that.mapping.edgeGroup]);
+				} else {
+					return '#000';
+				}
+			});
+				
+		// Update and restart the simulation.
+		that.simulation.nodes(nodes);
+		that.simulation.force("link").links(links);
+		that.simulation.alpha(1).restart();
 		
 		function mouseOver(){
-			var data = d3.select(this).data();
+			var data = d3.select(this).data()[0];
+			var textDisplay = data[that.mapping.nodeLabel]
 			
 			that.tooltip
-        .style("left", (d3.event.pageX - 10) + 'px')
+			  .style("left", (d3.event.pageX - 10) + 'px')
 			  .style("top", (d3.event.pageY - 30) + 'px')
-        .style("display", "inline-block")
+			  .style("display", "inline-block")
 			  .html(function(){
-				  return data[0].name;
+				  return textDisplay;
 			  });
 		}
 		
